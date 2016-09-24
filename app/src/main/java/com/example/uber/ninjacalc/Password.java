91b15f9d526ccc88f0ad327c22e7b67cc079eec5
Andre Vidal <andre_vidal@outlook.com>
2016-09-24 18:31:21 -0300

    Atualizacao troca de tela password

--- app/src/main/java/com/example/uber/ninjacalc/Password.java
+++ app/src/main/java/com/example/uber/ninjacalc/Password.java
@@ -1,7 +1,11 @@
 package com.example.uber.ninjacalc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class Password extends AppCompatActivity {
 
@@ -9,5 +13,101 @@ public class Password extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password);
+
+
+
+        final Intent it = new Intent(this,Menu.class);
+        final EditText password1;
+        final EditText password2;
+        final EditText password3;
+        final EditText password4;
+
+        password1 = (EditText) findViewById(R.id.password1);
+        password2 = (EditText) findViewById(R.id.password2);
+        password3 = (EditText) findViewById(R.id.password3);
+        password4 = (EditText) findViewById(R.id.password4);
+
+        password1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length() == 1) {
+                    password2.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+
+            }
+        });
+
+        password2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length() == 1) {
+                    password3.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        password3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length() == 1) {
+                    password4.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        password4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length() == 1) {
+                    startActivity(it);
+                    if (password1.getText().toString() + password2.getText().toString() + password3.getText().toString() + password4.getText().toString() == "1111"){
+                        startActivity(it);
+
+                    };
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
     }
 }
