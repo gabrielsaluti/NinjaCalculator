36161e9e65574260e71ce406a443ae8f92e7c3f4
Andre Vidal <andre_vidal@outlook.com>
2016-09-25 01:04:46 -0300

    Atualizacao Menu

---
+++ app/src/main/java/com/example/uber/ninjacalc/Documentos.java
@@ -0,0 +1,13 @@
+package com.example.uber.ninjacalc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Documentos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_documentos);
+    }
+}
